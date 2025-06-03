034c8019f05f3569ab576a5a67bacf2e34be46c0
Ellen <ellen.brage@welcom.se>
2025-06-03 17:09:30 +0200

    Slap some styling on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Spotify data converter</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div
+	class="flex h-full min-h-screen flex-col justify-center bg-gradient-to-tl from-green-600 to-green-200"
+>
+	<div class="mb-20 flex flex-col items-center justify-center">
+		<h1 class="text-4xl font-bold text-white">Spotify data converter</h1>
+	</div>
+</div>
