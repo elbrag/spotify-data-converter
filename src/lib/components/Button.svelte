2bce820ae2a9484ad6bee8993508054a529bcee1
Ellen <ellen.brage@welcom.se>
2025-06-28 14:53:09 +0200

    Create wip project structure, make a test button component and login component.

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let href: string | undefined = undefined;
+	export let onClick: () => void | undefined = () => undefined;
+	export let type: 'button' | 'submit' = 'button';
+	export let disabled: boolean = false;
+	export let className: string = '';
+
+	const classNames = `${className} py-2 px-4 bg-purple-400`;
+</script>
+
+{#if href}
+	<a {href} class="button {classNames}" class:disabled on:click|preventDefault={onClick}>
+		<slot>Default Link Text</slot>
+	</a>
+{:else}
+	<button {type} class="button {classNames}" {disabled} on:click={onClick}>
+		<slot>Default Button Text</slot>
+	</button>
+{/if}
---
+++ src/lib/components/Login.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Button from './Button.svelte';
+</script>
+
+<div class="login">
+	Login
+
+	<Button
+		onClick={() => {
+			console.log('log in');
+		}}>Log in</Button
+	>
+</div>
